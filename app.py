c5e959e50bfb71819b1f47a974eadb1929ccfb54
Daniil <danapak39@gmail.com>
2025-04-26 21:39:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def chat():
     return render_template('index.html', messages=messages)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
